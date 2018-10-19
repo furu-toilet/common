4217a50808c2cb9900ad47ed332dc6ed939f7c97
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-19 19:07:15 +0900

    Update info.php

--- info.php
+++ info.php
@@ -32,7 +32,7 @@ $selepass = $prepare->db_sql("select password  from user_info where id = '" . $_
   <tr>
     <td class="left">ユーザー名</td>
     <td class="center"><?php /*foreach($selename as $value){if($value['id'] == $id){echo $value['name']; break;}*/
-            echo $selename['name'];
+            echo $selename[0]['name'];
             }?></td>
     <td class="right"><a href="changename.html">ユーザー名の変更</a></td>
   </tr>  
